7138cb05db2c7e0bc352b3220621049ac901ef5c
royamuradova <royamuradova1998@gmail.com>
2024-09-24 01:54:13 +0000

    energy-drink-consumption code works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
-int main() 
-{
-  cout << " chapter 2 progrmming language\n "<< endl;
+
+int main() {
+    // Given data
+    int totalCustomers = 16500;
+    double percentEnergyDrinkers = 0.15;
+    double percentCitrusPref = 0.58;
+
+    // Calculate the number of customers purchasing one or more energy drinks per week
+    int energyDrinkCustomers = totalCustomers * percentEnergyDrinkers;
+
+    // Calculate the number of customers who prefer citrus-flavored energy drinks
+    int citrusPrefCustomers = energyDrinkCustomers * percentCitrusPref;
+
+    // Output the results
+    cout << "Approximate number of customers purchasing one or more energy drinks per week: " 
+         << energyDrinkCustomers << endl;
+    cout << "Approximate number of customers preferring citrus-flavored energy drinks: " 
+         << citrusPrefCustomers << endl;
+
+    return 0;
 }
